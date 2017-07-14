ef6a277221f7eaa76084766a7d1c549120b3457c
Kam <kammif1990@gmail.com>
2017-07-14 13:00:54 +0500

    finish

---
+++ src/components/Page.js
@@ -0,0 +1,27 @@
+import React, { PropTypes, Component } from 'react'
+
+export default class Page extends Component {
+    onYearBtnClick(e) {
+        this.props.getPhotos(+e.target.textContent)
+    }
+    render(){
+        const { year, photos, fetching } = this.props
+        return <div>
+            <p>
+                <button onClick={ ::this.onYearBtnClick }>2016</button>{' '}
+                <button onClick={ ::this.onYearBtnClick }>2015</button>{' '}
+                <button onClick={ ::this.onYearBtnClick }>2014</button>
+            </p>
+            <h3>{year} year</h3>
+            {
+                fetching ? <p>Loading...</p> : <p>You have { photos.length } photo</p>
+            }
+        </div>
+    }
+}
+
+Page.propTypes = {
+    year: PropTypes.number.isRequired,
+    photos: PropTypes.array.isRequired,
+    setYear: PropTypes.func.isRequired
+}
